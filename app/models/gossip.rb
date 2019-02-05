79b185449fae6ac3f56a7537b6b573c79a652c8e
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-02-05 12:03:46 +0100

    my changes between monday evening and Tuesday noon: gossip validations updated

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -4,4 +4,7 @@ class Gossip < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :commentaries, dependent: :destroy
   has_many :sub_commentaries, through: :commentaries, dependent: :destroy
+  validates :author, presence: true
+  validates :title, presence: true
+   
 end
