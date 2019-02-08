791a83e86d4d609e9cd3299bb00c98349ba6036c
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-02-08 09:10:47 +0100

    Gossip views test implemented

--- spec/views/gossips/index.html.erb_spec.rb
+++ spec/views/gossips/index.html.erb_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'gossips/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = FactoryBot.create(:user)
+    @gossip = FactoryBot.create(:gossip)
+    byebug
+  end
+
+  context 'it says The Gossip Project répertorie tous les potins de THP' do
+    it "displays 'The Gossip Project répertorie tous les potins de THP'" do
+      # génére la page
+      render
+
+      # le contenu "Bievenue" doit être dans la page
+      expect(rendered).to have_content 'The Gossip Project répertorie tous les potins de THP'
+    end
+  end
 end
--- spec/views/gossips/new.html.erb_spec.rb
+++ spec/views/gossips/new.html.erb_spec.rb
@@ -2,6 +2,49 @@
 
 require 'rails_helper'
 
+# frozen_string_literal: true
+
+require 'rails_helper'
+
 RSpec.describe 'gossips/new.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @test_gossip = FactoryBot.create(:gossip)
+  end
+
+  context 'it says Create a new potin !' do
+    it "displays 'Create a new potin !'" do
+      # génére la page
+      render
+
+      # le contenu "Bievenue" doit être dans la page
+      expect(rendered).to have_content 'Create a new potin !'
+    end
+  end
+
+  # context "it says 'En voilà un bon potin!' when the Gossip created is valid" do
+  #   it "displays 'welcome'" do
+  #     # dit à la view que @user sera le build d'un utilisateur avec "lol@email.com" comme email
+  #     @gossip = FactoryBot.create(:gossip)
+
+  #     # génère la vue (ceci doit être fait après avoir assigné la variable d'instance, pour qu'il puisse la trouver)
+  #     render
+
+  #     # la vue rendered doit afficher l'email passé
+  #     expect(rendered).to have_content "En voilà un bon potin!"
+  #   end
+  # end
+
+  # context "it says 'Raté!' when the Gossip created is invalid" do
+  #   it "displays 'welcome'" do
+
+  #     @gossip = FactoryBot.create(:gossip_skips_validate, title: '')
+  #     @gossip.valid? #compulsory otherwiser errors aren't generated.
+
+  #     # génère la vue (ceci doit être fait après avoir assigné la variable d'instance, pour qu'il puisse la trouver)
+  #     render
+
+  #     # la vue rendered doit afficher l'email passé
+  #     expect(rendered).to have_content "Raté!"
+  #   end
+  # end
 end
